5ba5efbdd57baa3920100f55530406e9fc2e5c78
Rob Measner <72046583+rob-cs@users.noreply.github.com>
2021-02-21 20:01:58 -0500

    very rough move forward/backward outline

--- boundary.ino
+++ boundary.ino
@@ -9,6 +9,60 @@ const int IN4 = /*pin number*/;
 #define IR_sensor_back A1 // rear senson
 int distance ;
 
+const byte Motor_A = 3;
+const byte Motor_B = 2;
+
+// Motor A
+int ENA = 10;
+int IN1 = 9;
+int IN2 = 8;
+
+// Motor B
+int ENB = 5;
+int IN3 = 6;
+int IN4 = 7;
+
+// Function to Move Forward
+void MoveForward(int dist, int motspeed) 
+{
+   // Set Motor A forward
+   digitalWrite(IN1, HIGH);
+   digitalWrite(IN2, LOW);
+ 
+   // Set Motor B forward
+   digitalWrite(IN3, HIGH);
+   digitalWrite(IN4, LOW);
+   
+   // Go forward until some point
+   //  while () {
+   //  }
+      
+   // Stop when done
+   analogWrite(ENA, 0);
+   analogWrite(ENB, 0);
+}
+ 
+// Function to Move in Reverse
+void MoveReverse(int dist, int motspeed) 
+{  
+  // Set Motor A reverse
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+ 
+  // Set Motor B reverse
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+   
+   // Go in reverse until some point
+   // while () {
+   // 
+   // }
+    
+   // Stop when done
+   analogWrite(ENA, 0);
+   analogWrite(ENB, 0);
+}
+
 void setup() {
   Serial.begin(9600);
   // delay?
